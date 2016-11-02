399a2e71eb97d4bba1885486597affb9981e22b5
MSI\lpack <l.packster@gmail.com>
2016-11-02 11:11:16 -0600

    Views and controllers

---
+++ ScheduleCreator/Controllers/ClassroomsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ScheduleCreator.Models;
+
+namespace ScheduleCreator.Controllers
+{
+    public class ClassroomsController : Controller
+    {
+        private ScheduleCreaterEntities db = new ScheduleCreaterEntities();
+
+        // GET: Classrooms
+        public ActionResult Index()
+        {
+            var classrooms = db.Classrooms.Include(c => c.Building);
+            return View(classrooms.ToList());
+        }
+
+        // GET: Classrooms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classroom classroom = db.Classrooms.Find(id);
+            if (classroom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classroom);
+        }
+
+        // GET: Classrooms/Create
+        public ActionResult Create()
+        {
+            ViewBag.building_id = new SelectList(db.Buildings, "building_id", "buildingPrefix");
+            return View();
+        }
+
+        // POST: Classrooms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "classroom_id,building_id,buildingPrefix,roomNumber,classroomCapacity,computers,availableFromTime,availableToTime,active")] Classroom classroom)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Classrooms.Add(classroom);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.building_id = new SelectList(db.Buildings, "building_id", "buildingPrefix", classroom.building_id);
+            return View(classroom);
+        }
+
+        // GET: Classrooms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classroom classroom = db.Classrooms.Find(id);
+            if (classroom == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.building_id = new SelectList(db.Buildings, "building_id", "buildingPrefix", classroom.building_id);
+            return View(classroom);
+        }
+
+        // POST: Classrooms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "classroom_id,building_id,buildingPrefix,roomNumber,classroomCapacity,computers,availableFromTime,availableToTime,active")] Classroom classroom)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(classroom).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.building_id = new SelectList(db.Buildings, "building_id", "buildingPrefix", classroom.building_id);
+            return View(classroom);
+        }
+
+        // GET: Classrooms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classroom classroom = db.Classrooms.Find(id);
+            if (classroom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classroom);
+        }
+
+        // POST: Classrooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Classroom classroom = db.Classrooms.Find(id);
+            db.Classrooms.Remove(classroom);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
