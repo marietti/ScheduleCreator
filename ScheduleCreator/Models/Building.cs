e28a8b7b4e50bec46c383981ce5017261e05ba86
nick <nickmarietti@mail.weber.edu>
2016-12-02 17:33:29 -0700

    Added required fields to the building model to keep the create building from crashing with null fields.

--- ScheduleCreator/Models/Building.cs
+++ ScheduleCreator/Models/Building.cs
@@ -11,7 +11,7 @@ namespace ScheduleCreator.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
     public partial class Building
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,10 +19,13 @@ namespace ScheduleCreator.Models
         {
             this.Classrooms = new HashSet<Classroom>();
         }
-    
+
         public int building_id { get; set; }
+        [Required(ErrorMessage = "A Building Prefix is required")]
         public string buildingPrefix { get; set; }
+        [Required(ErrorMessage = "A Building Name is required")]
         public string buildingName { get; set; }
+        [Required(ErrorMessage = "A Campus Prefix is required")]
         public string campusPrefix { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
