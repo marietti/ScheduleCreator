ac8df3a074d66d2511d379821a6ed577bc6d2fd9
Jeremy Ottesen <jeottesen@gmail.com>
2016-12-05 15:05:09 -0700

    Move Event creation to the controller

--- ScheduleCreator/Controllers/InstructorsController.cs
+++ ScheduleCreator/Controllers/InstructorsController.cs
@@ -7,6 +7,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ScheduleCreator.Models;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
 
 namespace ScheduleCreator.Controllers
 {
@@ -27,6 +29,43 @@ namespace ScheduleCreator.Controllers
 
         public ActionResult InstructorCalendar()
         {
+            Dictionary<int, string> instructorEvents = new Dictionary<int, string>();
+            foreach (Instructor instructor in db.Instructors)
+            {
+                foreach (Section section in instructor.Sections)
+                {
+                    if (section.daysTaught != null)
+                    {
+                        List<Calendar.Event> events = new List<Calendar.Event>();
+                        foreach (char day in section.daysTaught)
+                        {
+                            string daysToAdd = "0";
+                            daysToAdd = char.ToLower(day) == 'm' ? "1" : daysToAdd;
+                            daysToAdd = char.ToLower(day) == 't' ? "2" : daysToAdd;
+                            daysToAdd = char.ToLower(day) == 'w' ? "3" : daysToAdd;
+                            daysToAdd = char.ToLower(day) == 'r' ? "4" : daysToAdd;
+                            daysToAdd = char.ToLower(day) == 'f' ? "5" : daysToAdd;
+                            daysToAdd = char.ToLower(day) == 's' ? "6" : daysToAdd;
+
+                            string date = "0" + daysToAdd + "-01-1900";
+                            string startTime = section.courseStartTime.ToString();
+                            string endTime = section.courseEndTime.ToString();
+                            startTime = date + " " + startTime;
+                            endTime = date + " " + endTime;
+
+                            string id = section.section_id.ToString();
+                            string title = section.coursePrefix + section.courseNumber + "<br/>" + section.buildingPrefix + section.roomNumber;
+                            string start = startTime;
+                            string end = endTime;
+                            Calendar.Event temp = new Calendar.Event(id, title, start, end);
+                            events.Add(temp);
+                        }
+                        string jsonEvents = JsonConvert.SerializeObject(events);
+                        instructorEvents.Add(section.section_id, jsonEvents);
+                    }
+                }
+            }
+            ViewBag.instructorEvents = instructorEvents;
             return View(db.Instructors.ToList());
         }
 
---
+++ ScheduleCreator/Models/Calendar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScheduleCreator.Models
+{
+    public class Calendar
+    {
+        public class Event
+        {
+            public string id { get; set; }
+            public string title { get; set; }
+            public string start { get; set; }
+            public string end { get; set; }
+            public string backgroundColor { get; set; }
+            public string textColor { get; set; }
+
+            public Event(string id, string title, string start, string end)
+            {
+                this.id = id;
+                this.title = title;
+                this.start = start;
+                this.end = end;
+                backgroundColor = "#492365";
+                textColor = "#FFF";
+            }
+        }
+    }
+}
