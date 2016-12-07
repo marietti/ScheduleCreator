2f08aada38760d324a1b48f2fd44bf6fa5b49d2f
bomber916 <jaredwilliams@mail.weber.edu>
2016-12-07 09:51:15 -0700

    Fixed issue on section index

--- ScheduleCreator/App_Start/RouteConfig.cs
+++ ScheduleCreator/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ScheduleCreator
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Instructors", action = "SectionByInstructor", id = UrlParameter.Optional }
             );
         }
     }
